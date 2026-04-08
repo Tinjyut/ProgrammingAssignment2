e7aa8988b0c63d6469199b00ec91731bdfcea6b4
LIN TIANYUE <linty0924@gmail.com>
2026-04-09 01:08:26 +0800

    Create cachesolve.R

---
+++ cachesolve.R
@@ -0,0 +1,39 @@
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  get <- function() x
+  
+  setinverse <- function(inverse) inv <<- inverse
+  
+  getinverse <- function() inv
+  
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+cacheSolve <- function(x, ...) {
+  inv <- x$getinverse()
+  
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setinverse(inv)
+  inv
+}
+
+m <- matrix(c(1,2,3,4), 2, 2)
+cm <- makeCacheMatrix(m)
+
+cacheSolve(cm)
+
+cacheSolve(cm)
